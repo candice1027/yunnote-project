606259d4c32ab3b1082029d0ec4dde4e76337359
lvsuzhen <lvsz@boqii.com>
2019-03-13 18:31:47 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/login'
+import NotebookList from '@/components/NotebookList'
+import NotebookDetail from '@/components/NoteDetail'
+import TrashDetail from '@/components/TrashDetail'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login/:user',
+      name: 'Login',
+      component: Login,
     },
     {
-      path: '/login',
+      path: '/login/:user',
       name: 'Login',
-      component: Login
+      component: Login,
+    },
+    {
+      path: '/login/:user',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/login/:user',
+      name: 'Login',
+      component: Login,
     },
   ]
 })
