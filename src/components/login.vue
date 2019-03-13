0379b4526a0bd60759c80d7de14b72826acf7745
lvsuzhen <lvsz@boqii.com>
2019-03-13 17:37:48 +0800

    搭建项目框架

---
+++ src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world333'
+    }
+  }
+  
+}
+</script>
+<style lang="" scoped>
+div {
+  color: red;
+}
+  
+</style>
